7cff3008747682ff1ab242f2862ed6449fcca43c
Rahul Deokate <rahuldeokate47666@gmail.com>
2025-08-16 14:18:48 +0200

    Added variables of jenkins server

---
+++ terraform/jenkins-server/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_id" {
+  description = "vpc id of the project"
+}
+
+variable "ami" {
+  description = "name of ami"
+}
+
+variable "instance_type" {
+  description = "instance type of jenkins server"
+  type = list(string)
+  default = ["t2.micro"] 
+}
+
+variable "subnet_1" {
+  description = "subnet for jenkins server"
+}
+
+variable "key_name" {
+  description = "key name of server"
+}
